5380517abeec2849af08225d24de811221a54bbf
EpicRaisin <65032841+LameGrape@users.noreply.github.com>
2022-10-18 16:40:39 -0500

    Forked from polynode

--- main.js
+++ main.js
@@ -1,16 +1,45 @@
 const { app } = require("electron")
 const electron = require("electron")
+const path = require("path")
 
-function createWindow(file, options) {
-    const window = new electron.BrowserWindow(options)
-    window.loadFile(file)
+const createWindow = () => {
+    const window = new electron.BrowserWindow({
+        frame: false,
+        width: 1068,
+        height: 600,
+        minHeight: 40,
+        minWidth: 300,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
+        },
+        icon: path.join(__dirname, 'build/icon.png')
+    })
+    const ipc = electron.ipcMain
+    ipc.on("maximize", () => {
+        if (window.isMaximized()) {
+            window.unmaximize()
+        } else {
+            window.maximize()
+        }
+    })
+    ipc.on("minimize", () => {
+        window.minimize()
+    })
+    ipc.on("close", () => {
+        app.quit()
+    })
+
+    window.webContents.setWindowOpenHandler(details => {
+        require('electron').shell.openExternal(details.url)
+        return { action: 'deny' }
+    })
+
+    window.loadFile("src/index.html")
 }
 
 app.whenReady().then(() => {
-    createWindow("src/index.html", {
-        width: 800,
-        height: 600
-    })
+    createWindow()
 })
 
 app.on("window-all-closed", () => {
